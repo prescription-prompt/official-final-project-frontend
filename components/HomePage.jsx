aec198e56d1bb51d6a6574a7f4d7a079be0ba1e0
MichalLewczuk <lewczukmichael84@gmail.com>
2022-05-11 17:01:50 +0100

    add-next-prompt-render-fix-medcalendar

--- components/HomePage.jsx
+++ components/HomePage.jsx
@@ -5,10 +5,11 @@ import { useState } from 'react';
 import { ScrollView, View } from 'react-native-web';
 
 export default function HomePage({ setPage, user }) {
+  const [Reminders, setReminders] = useState([]);
   return (
     <>
-      <NextPrompt />
-      <MedCalendar user={user} />
+      <NextPrompt Reminders={Reminders} />
+      <MedCalendar user={user} setReminders={setReminders} Reminders={Reminders} />
       <NavButtons setPage={setPage} />
     </>
   );
--- components/NextPrompt.jsx
+++ components/NextPrompt.jsx
@@ -1,11 +1,20 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
 import { GeneralStyles, NextPromptStyles } from '../styles/Styles';
+import msToTime from '../utils/msToTime';
 
-export default function NextPrompt() {
+export default function NextPrompt({ Reminders }) {
   const [MedicationName, setMedicationName] = useState('MedicationName');
   const [MedicationDosage, setMedicationDosage] = useState('XXmg');
   const [Timer, setTimer] = useState('XX');
+
+  useEffect(() => {
+    setMedicationDosage(Reminders?.[0]?.props?.reminder.dosage);
+    setMedicationName(Reminders?.[0]?.props?.reminder.name);
+    const tD = new Date(Reminders?.[0]?.props?.time);
+    const timestampDate = new Date(Reminders?.[0]?.props?.time + tD.getTimezoneOffset() * 60000);
+    setTimer(msToTime(timestampDate - Date.now()));
+  }, [Reminders]);
 
   return (
     <View style={GeneralStyles.card}>
@@ -14,7 +23,7 @@ export default function NextPrompt() {
       <Text style={[GeneralStyles.fontLarge, GeneralStyles.white]}>{MedicationName}</Text>
       <View style={[GeneralStyles.flex, GeneralStyles.flexRow]}>
         <Text style={GeneralStyles.white}>in </Text>
-        <Text style={[GeneralStyles.fontLarge, GeneralStyles.white]}>{Timer} minutes</Text>
+        <Text style={[GeneralStyles.fontLarge, GeneralStyles.white]}>{Timer}</Text>
       </View>
     </View>
   );
